e4800f1a424975d13460c77789ef8a9888196744
Jean George <jean.george.alves@gmail.com>
2023-09-08 15:11:50 -0300

    feat: add .prettierrc and fix styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import { Footer, Header } from "./components"
-import Routes from "./routes"
+import { Footer, Header } from './components'
+import Routes from './routes'
 
 const App = () => {
   return (
-    <div className="font-poppins xs:py-2 sm:py-10 xs:px-1 sm:px-2 md:px-4 lg:px-8 xl:px-16 2xl:px-32 flex flex-col h-screen gap-52">
+    <div className="font-poppins py-2 px-2 sm:px-8 flex flex-col h-screen gap-52">
       <Header />
       <Routes />
       <Footer />
