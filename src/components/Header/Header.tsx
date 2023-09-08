da8cd3c8a4a897c07443c7e9be22adf931aff481
jeangeorge <jean.george.alves@gmail.com>
2023-09-08 07:14:04 -0300

    fix: navbar mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,130 @@
 import { Link } from "react-router-dom"
 import { GithubIcon, LinkedinIcon, LogoIcon } from "../../assets/icons"
+import { useState } from "react"
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const toggle = () => {
+    setOpen(!open)
+  }
+
   return (
-    <header className="xs:hidden sm:flex items-center justify-between">
-      <Link to="/">
-        <LogoIcon />
-      </Link>
-
-      <nav className="flex xs:gap-1 sm:gap-2 md:gap-8 text-xl text-primary">
-        <Link to="/" className="hover:text-gradient">
-          Home
-        </Link>
-        <Link to="/about" className="hover:text-gradient">
-          About
-        </Link>
-        <Link to="/stack" className="hover:text-gradient">
-          Stack
-        </Link>
-        <Link to="/projects" className="hover:text-gradient">
-          Projects
-        </Link>
-        <Link to="/contact" className="hover:text-gradient">
-          Contact
-        </Link>
-      </nav>
-
-      <div className="flex xs:gap-2 md:gap-4">
-        <Link to="https://github.com/jeangeorge" target="_blank">
-          <GithubIcon />
-        </Link>
-        <Link to="https://www.linkedin.com/in/jean-george/" target="_blank">
-          <LinkedinIcon />
-        </Link>
+    <nav>
+      <div className="relative flex h-16 items-center justify-between">
+        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <button
+            type="button"
+            className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gradient hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            aria-controls="mobile-menu"
+            aria-expanded="false"
+            onClick={toggle}
+          >
+            <span className="absolute -inset-0.5"></span>
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="block h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+            <svg
+              className="hidden h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start w-full">
+          <div className="flex flex-shrink-0 items-center">
+            <Link to="/">
+              <LogoIcon />
+            </Link>
+          </div>
+          <div className="hidden sm:ml-6 sm:block w-full">
+            <div className="flex items-center justify-center space-x-4 text-primary md:text-xl w-full">
+              <Link to="/" className="px-3 py-2 hover:text-gradient">
+                Home
+              </Link>
+              <Link to="/about" className="px-3 py-2 hover:text-gradient">
+                About
+              </Link>
+              <Link to="/stack" className="px-3 py-2 hover:text-gradient">
+                Stack
+              </Link>
+              <Link to="/projects" className="px-3 py-2 hover:text-gradient">
+                Projects
+              </Link>
+              <Link to="/contact" className="px-3 py-2 hover:text-gradient">
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="absolute inset-y-0 right-0 flex gap-3 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <Link to="https://github.com/jeangeorge" target="_blank">
+            <GithubIcon />
+          </Link>
+          <Link to="https://www.linkedin.com/in/jean-george/" target="_blank">
+            <LinkedinIcon />
+          </Link>
+        </div>
       </div>
-    </header>
+
+      {open && (
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            <Link
+              to="/"
+              className="text-primary hover:bg-gradient hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="text-primary hover:bg-gradient hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              About
+            </Link>
+            <Link
+              to="/stack"
+              className="text-primary hover:bg-gradient hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Stack
+            </Link>
+            <Link
+              to="/projects"
+              className="text-primary hover:bg-gradient hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Projects
+            </Link>
+            <Link
+              to="/contact"
+              className="text-primary hover:bg-gradient hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Projects
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
   )
 }
 
